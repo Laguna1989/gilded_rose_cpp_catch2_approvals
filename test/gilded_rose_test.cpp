2a2997023593016ea8c89b49d0dfd4d57636f63c
Simon Weis <laguna.ws@gmail.com>
2024-04-09 17:42:50 +0200

    Use nice test code

--- test/gilded_rose_test.cpp
+++ test/gilded_rose_test.cpp
@@ -4,52 +4,29 @@
 #include <catch2/catch_all.hpp>
 #include <utility>
 
-namespace {
-
-struct TestData {
-    std::vector<Item> item;
-    int daysToUpgrade {};
-};
-
-std::ostream& operator<<(std::ostream& os, TestData const& obj)
+std::ostream& operator<<(std::ostream& os, Item const& obj)
 {
-    return os << "name: " << obj.item[0].name << ", sellIn: " << obj.item[0].sellIn
-              << ", quality: " << obj.item[0].quality << ", daysToUpgrade: " << obj.daysToUpgrade;
+    return os << "name: " << obj.name << ", sellIn: " << obj.sellIn
+              << ", quality: " << obj.quality;
 }
-
-std::vector<TestData> getAllTestDataCombinations()
-{
-    std::vector<TestData> testData;
-
-    // clang-format off
-    for (auto name : { "foo", "Sulfuras, Hand of Ragnaros", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert" }) {
-        // clang-format on
-        for (auto sell_in : { -10, -5, -1, 0, 1, 2, 5, 10, 20, 50, 100 }) {
-            for (auto quality : { 0, 1, 2, 5, 10, 20, 50, 100 }) {
-                for (auto daysToUpgrade : { 0, 1, 2, 5, 10 }) {
-                    TestData data;
-                    data.item.emplace_back(name, sell_in, quality);
-                    data.daysToUpgrade = daysToUpgrade;
-                    testData.emplace_back(std::move(data));
-                }
-            }
-        }
-    }
-    return testData;
-}
-
-} // namespace
 
 TEST_CASE("Update Quality")
 {
-    std::vector<TestData> items = getAllTestDataCombinations();
-
-    for (auto& testData : items) {
-        GildedRose gildedRose { testData.item };
-        for (auto i = 0; i != testData.daysToUpgrade; ++i) {
-            gildedRose.updateQuality();
+    auto const f = [](const std::string& name, int sellIn, int quality, int days) {
+        std::vector<Item> items = { Item(name, sellIn, quality) };
+        GildedRose app(items);
+        for (int day = 0; day != days; ++day) {
+            app.updateQuality();
         }
-    }
+        return items[0];
+    };
+    // clang-format off
+    std::vector<std::string> const names = {"Foo", "+5 Dexterity Vest", "Aged Brie", "Elixir of the Mongoose", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert"};
+    // clang-format on
 
-    ApprovalTests::Approvals::verifyAll(items);
+    std::vector<int> sellIns = { -10, -5, -1, 0, 1, 2, 5, 10, 20, 50, 100 };
+    std::vector<int> qualities = { 0, 1, 2, 5, 10, 20, 50, 100 };
+    std::vector<int> days ={ 0, 1, 2, 5, 10 };
+
+    ApprovalTests::CombinationApprovals ::verifyAllCombinations(f, names, sellIns, qualities, days);
 }
