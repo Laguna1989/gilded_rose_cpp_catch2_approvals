510f18a33c60de0cf9dd385c2a8e14fc1cc44a10
Weis, Simon <weis@e-laborate.net>
2024-11-04 16:17:35 +0100

    remove all approval test solution code

--- test/gilded_rose_test.cpp
+++ test/gilded_rose_test.cpp
@@ -3,29 +3,8 @@
 #include <ApprovalTests.hpp>
 #include <catch2/catch_test_macros.hpp>
 
-std::ostream& operator<<(std::ostream& os, Item const& obj)
+
+TEST_CASE("Empty test case")
 {
-    return os << "name: " << obj.name << ", sellIn: " << obj.sellIn
-              << ", quality: " << obj.quality;
-}
-
-TEST_CASE("Update Quality")
-{
-    auto const f = [](const std::string& name, int sellIn, int quality, int days) {
-        std::vector<Item> items = { Item(name, sellIn, quality) };
-        GildedRose app(items);
-        for (int day = 0; day != days; ++day) {
-            app.updateQuality();
-        }
-        return items[0];
-    };
-    // clang-format off
-    std::vector<std::string> const names = {"Foo", "+5 Dexterity Vest", "Aged Brie", "Elixir of the Mongoose", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert"};
-    // clang-format on
-
-    std::vector<int> sellIns = { -10, -5, -1, 0, 1, 2, 5, 10, 20, 50, 100 };
-    std::vector<int> qualities = { 0, 1, 2, 5, 10, 20, 50, 100 };
-    std::vector<int> days ={ 0, 1, 2, 5, 10 };
-
-    ApprovalTests::CombinationApprovals ::verifyAllCombinations(f, names, sellIns, qualities, days);
+    REQUIRE(true);
 }
