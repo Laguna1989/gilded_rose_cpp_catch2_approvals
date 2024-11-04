47c7c6215a2ef7906260b5c109b3fc77dc2cbad6
Weis, Simon <weis@e-laborate.net>
2024-11-04 16:26:38 +0100

    first single item test

--- test/gilded_rose_test.cpp
+++ test/gilded_rose_test.cpp
@@ -3,8 +3,18 @@
 #include <ApprovalTests.hpp>
 #include <catch2/catch_test_macros.hpp>
 
-
-TEST_CASE("Empty test case")
+std::ostream& operator<<(std::ostream& os, Item const& obj)
 {
-    REQUIRE(true);
+    return os << "name: " << obj.name << ", sellIn: " << obj.sellIn
+              << ", quality: " << obj.quality;
+}
+
+TEST_CASE("Single item")
+{
+    std::vector<Item> items {Item{"Axe", 10, 15}};
+    GildedRose gildedRose{items};
+
+    gildedRose.updateQuality();
+
+    ApprovalTests::Approvals::verify(items[0]);
 }
