677c9412feb9ac12806812f1690c46f0b2aa7bfc
Weis, Simon <weis@e-laborate.net>
2024-11-05 08:34:53 +0100

    Better formatting of names

--- test/gilded_rose_test.cpp
+++ test/gilded_rose_test.cpp
@@ -6,13 +6,12 @@
 
 std::ostream& operator<<(std::ostream& os, Item const& obj)
 {
-    return os << "name: " << obj.name << ", sellIn: " << obj.sellIn
-              << ", quality: " << obj.quality;
+    return os << "name: " << obj.name << ", sellIn: " << obj.sellIn << ", quality: " << obj.quality;
 }
 
 TEST_CASE("Update Quality")
 {
-    auto const f = [](const std::string& name, int sellIn, int quality, int days) {
+    auto const f = [](std::string const& name, int sellIn, int quality, int days) {
         std::vector<Item> items = { Item(name, sellIn, quality) };
         GildedRose app(items);
         for (int day = 0; day != days; ++day) {
@@ -21,12 +20,19 @@ TEST_CASE("Update Quality")
         return items[0];
     };
     // clang-format off
-    std::vector<std::string> const names = {"Foo", "+5 Dexterity Vest", "Aged Brie", "Elixir of the Mongoose", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert"};
+    std::vector<std::string> const names = {
+	"Foo",
+	"+5 Dexterity Vest",
+	"Aged Brie",
+	"Elixir of the Mongoose",
+	"Sulfuras, Hand of Ragnaros",
+	"Backstage passes to a TAFKAL80ETC concert"
+};
     // clang-format on
 
     std::vector<int> sellIns = { -10, -5, -1, 0, 1, 2, 5, 10, 20, 50, 100 };
     std::vector<int> qualities = { 0, 1, 2, 5, 10, 20, 50, 100 };
-    std::vector<int> days ={ 0, 1, 2, 5, 10 };
+    std::vector<int> days = { 0, 1, 2, 5, 10 };
 
     ApprovalTests::CombinationApprovals ::verifyAllCombinations(f, names, sellIns, qualities, days);
 }
